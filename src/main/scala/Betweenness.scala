0af6feea06193ed88fc8d7b4b174065b29624100
li-zhaoyang <38060695+li-zhaoyang@users.noreply.github.com>
2018-04-04 16:18:23 -0700

    Betweenness Graph Constructed.(correctness not verified)

--- src/main/scala/Betweenness.scala
+++ src/main/scala/Betweenness.scala
@@ -29,23 +29,143 @@ object Betweenness {
       .keys
       .map(a => (a.toLong, a))
 
+//    val userIDs =
+//      users
+//        .map(_._1)
+//        .collect()
+    val emptyEdgeRDD = RDD[Edge[Double]]()
     val relationships1 =
       userItemSet
         .cartesian(userItemSet)
         .filter(a => a._1._1 != a._2._1)
         .map(a => (a._1._1, a._2._1, a._1._2.intersect(a._2._2).size))
         .filter(_._3 >= 7)
-        .map(a => (a._1, a._2, 0))
         .cache()
 
-    val relationships: RDD[Edge[(VertexId, Int)]] =
+    val relationships: RDD[Edge[Int]] =
       relationships1
         .union(relationships1.map(a => (a._2, a._1, a._3)))      //two directed edges for one undirected edge
-        .map(a => Edge(a._1.toLong, a._2.toLong, (-100L, a._3)))
+        .map(a => Edge(a._1.toLong, a._2.toLong, 0))
     val defaultUser = -100
     // Build the initial Graph
-    val graph = Graph(users, relationships, defaultUser)
+    val initialGraph: Graph[Int, Int] =
+      Graph(users, relationships, defaultUser)
+        .cache()
+
+    def vertexToBfsGraphFunFromGraph(graph: Graph[Int, Int], users: RDD[(VertexId, Int)]): ((VertexId, Int)) => Graph[Int, Double] = {
+      def fun(a: (VertexId, Int)): Graph[Int, Double] = {
+        var travelledSet = Set(a._1)
+        var lastLevelSet = Set(a._1)
+        var remainEdgeRDD: RDD[Edge[Double]] = RDD[Edge[Double]]()
+        var vertexWeightMap =
+          users
+            .map(_._1)
+            .collect()
+            .map(a => (a, 0.0))
+            .toMap
+            .updated(a._1, 1.0)
+        while (lastLevelSet.nonEmpty) {
+          val thisLevelEdges =
+            graph
+              .mapEdges[Double](a => 0.0)
+              .edges
+            .filter(a => lastLevelSet.contains(a.srcId))
+            .filter(a => !travelledSet.contains(a.dstId))
+          lastLevelSet =
+            thisLevelEdges
+              .map(_.dstId)
+              .collect()
+              .toSet
+          thisLevelEdges.foreach(a => {
+            vertexWeightMap = vertexWeightMap.updated(
+              a.dstId,
+              vertexWeightMap.getOrElse(a.dstId, 0.0) + vertexWeightMap.getOrElse(a.srcId, 1.0)
+            )
+          })
+          travelledSet = travelledSet.union(lastLevelSet)
+          remainEdgeRDD = remainEdgeRDD.union(thisLevelEdges)
+        }
+        var bfsTreeGraph = Graph(users, remainEdgeRDD)
+        var bottomUpLastSet = bfsTreeGraph.outDegrees.filter(_._2 == 0).map(_._1).collect().toSet
+        val inDegreeMap = bfsTreeGraph.inDegrees.collect().toMap
+        while (bottomUpLastSet.nonEmpty) {
+          val thisEdges =
+            bfsTreeGraph
+              .edges
+              .filter(a => bottomUpLastSet.contains(a.dstId))
+              .map(a =>
+                Edge(a.srcId,
+                  a.dstId,
+                  vertexWeightMap.getOrElse(a.dstId, 1.0) / inDegreeMap.getOrElse(a.dstId, 1).toDouble
+                )
+              )
+          thisEdges
+            .foreach(a =>
+              vertexWeightMap =
+                vertexWeightMap
+                  .updated(
+                    a.srcId,
+                    vertexWeightMap.getOrElse(a.srcId, 1) + a.attr
+                  )
+            )
+          bottomUpLastSet =
+            thisEdges
+              .map(_.srcId)
+              .collect()
+              .toSet
+          bfsTreeGraph =
+            Graph(
+              bfsTreeGraph
+                .vertices,
+              bfsTreeGraph
+                .edges
+                .union(thisEdges)
+            )
+        }
+
+        bfsTreeGraph.groupEdges((a, b) => a + b)
+      }
+      fun
+    }
+    var bfsGraphsCombined = Graph[Int, Double](initialGraph.vertices, emptyEdgeRDD)
+    initialGraph
+      .vertices
+      .collect()
+      .foreach(
+        a => {
+          val bfsGraphFromVertex = vertexToBfsGraphFunFromGraph(initialGraph, users)(a)
+          bfsGraphsCombined = Graph(bfsGraphFromVertex.vertices, bfsGraphsCombined.edges.union(bfsGraphFromVertex.edges))
+        }
+      )
+    val bfsGraphLargeToSmall =
+      Graph(
+        bfsGraphsCombined
+          .vertices,
+        bfsGraphsCombined
+          .edges
+          .filter(a => a.srcId > a.srcId)
+      )
+    bfsGraphsCombined =
+      Graph(
+        bfsGraphsCombined.vertices,
+        bfsGraphsCombined
+          .edges
+          .filter(a => a.srcId <= a.dstId)
+          .union(
+            bfsGraphLargeToSmall
+              .reverse
+              .edges
+          )
+      )
+        .groupEdges((a, b) => a + b)
+
+
+
+
 
 
   }
 }
+
+
+//use groupEdges in last step for betweenness
